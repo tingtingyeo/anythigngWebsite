73a1cb0fc407a1065e4e2aaeaae09533da35d2df
Shi Ting <23043434@myrp.edu.sg>
2025-07-04 01:27:34 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Add animated heading
+    const header = document.querySelector('.header');
+    if (header) {
+        header.innerHTML = 'Anythingg Tourism Agency <span class="wave">🌴</span>';
+    }
+    // No further JS needed as all content is now in HTML
+});
